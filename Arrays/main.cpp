8c9bab9aea528d093e9f1be3663fff6ad2826e5d
Екатерина <@WIN-3DUOBH7JJ7D>
2023-01-21 19:34:19 +0300

    Add project files.

---
+++ Arrays/main.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+using namespace std;
+void main()
+{
+	setlocale(LC_ALL, "");
+	const int n = 5;
+	int arr[n];
+	//arr[2] = 1024;
+	//cout << arr[2] << endl;
+	cout << "Введите элемент:";
+	for (int i = 0; i < n; i++)
+	{
+		cin>> arr[i];
+	}
+	//Вывод массива на экран в прямом порядке
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i]<<"\t";
+	}
+	cout << endl;
+	//Вывод массива на экран в обратном порядке
+		for (int i = n-1; i>=0 ; i--)
+	{
+			cout << arr[i] << "\t";
+	}
+	cout << endl;
+	//Вычисление суммы элементов массива
+
+		int sum = 0;
+		for (int i = 0; i < n; i++)
+		{
+			sum += arr[i];
+		}
+		cout << sum;
+		cout << endl;
+		//Вычисление среднего арифмитического
+		int arithmeticmean = sum / n;
+		cout << arithmeticmean;
+		cout << endl;
+		//Вывод максимального и минимального значения
+		//
+	}	
+
---
+++ Shift/main.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+using namespace std;
+void main()
+{
+	setlocale(LC_ALL, "");
+	
+	const int n = 10;
+	int mass[n] = { 0,	1,	2,	3,	4,	5,	6,	7,	8,	9 };
+	for (int i = 0; i < n; i++)
+	{
+		cout << mass[i] << "\t";
+	}
+	int t = mass[0];
+	int a = mass[1];
+	int c = mass[2];
+	cout << endl;
+	//cout << "ввести количество сдвига влево:"; cin >> N;
+	for (int i = 0; i < n - 1; i++)
+	{
+		mass[i] = mass[i+1];
+	}
+	mass[9] = t;
+	for (int i = 0; i < n; i++)
+	{
+		cout << mass[i] << "\t";
+	}
+	cout << endl;
+	for (int i = 0; i < n - 1; i++)
+	{
+		mass[i] = mass[i + 1];
+	}
+	mass[9] = a;
+	for (int i = 0; i < n; i++)
+	{
+		cout << mass[i] << "\t";
+	}
+	cout << endl;
+	for (int i = 0; i < n - 1; i++)
+	{
+		mass[i] = mass[i + 1];
+	}
+	mass[9] = c;
+	for (int i = 0; i < n; i++)
+	{
+		cout << mass[i] << "\t";
+	}
+	cout << endl;
+
+}
